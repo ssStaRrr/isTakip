ff1876ed3d3e719f270c8f0ee2500cf2e9a40731
ssStaRrr <ibrahimsasmaz3416@hotmail.com>
2024-10-12 18:21:02 +0300

    add home page and login page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const {connectDB} = require("./DB/connectDB")
+const path = require('path');
 
 // Middleware
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
+
+// Define the directory for serving static files (like HTML)
+const publicDirectoryPath = path.join(__dirname, 'public')
+
+// Set up middleware to serve static files from the 'public' directory
+app.use(express.static(publicDirectoryPath));
 
 connectDB()
     .then((message) => {
@@ -16,4 +23,8 @@ connectDB()
     })
     .catch(err => {
         console.log(err)
+    })
+
+    app.get("/", (req,res) => {
+        res.sendFile(path.join(publicDirectoryPath, "home.html"))
     })
